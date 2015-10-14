d0728e21c6104379bbd22ddae52022738d3ab5be
Razvan Mihai Tache <rtache@tremend.ro>
2015-10-14 23:18:36 +0300

    Refactor the code, and made a templateing system for the data.

--- Projects/Dashboard/scripts/functions.js
+++ Projects/Dashboard/scripts/functions.js
@@ -20,3 +20,44 @@ function createElement(element, container, attributes, properties) {
 
     return el;
 }
+
+function createElementFromObject(data, value) {
+    data["container"] = data["container"] || null;
+    data["element"] = data["element"] || "span";
+    data["attributes"] = data["attributes"] || {};
+    data["properties"] = data["properties"] || {};
+    if (value) {
+        data["properties"]["textContent"] = value;
+    }
+
+    return createElement(data["element"], data["container"], data["attributes"], data["properties"]);
+}
+
+function printItemProperties(item, properties, parentElement) {
+    for (var prop in properties) {
+        var type = properties[prop]["type"];
+        var attribute = properties[prop]['value'];
+        var printData = properties[prop]['element'];
+
+        var element = createElementFromObject(printData, item[attribute]);
+        if (type === "primitive") {
+            parentElement.appendChild(element);
+        } else if (type === "array") {
+            var grandFather = printData["container"] ? element.children[0] : element;
+
+            for (var i = 0; i < item[prop].length; i++) {
+
+                var newParent = createElementFromObject(properties[prop]['separator']);
+                printItemProperties(item[prop][i], attribute, newParent.children[0]);
+                grandFather.appendChild(newParent);
+
+            }
+
+            parentElement.appendChild(element);
+        } else if (type === "object") {
+            var newParent = printData["container"] ? element.children[0] : element;
+            printItemProperties(item[prop], attribute, newParent);
+            parentElement.appendChild(element)
+        }
+    }
+}
--- Projects/Dashboard/scripts/main.js
+++ Projects/Dashboard/scripts/main.js
@@ -20,20 +20,124 @@ for (var i = 0; i < items.length; i++) {
     var itemContainer = document.createElement("li");
     var basicDataContainer = document.createElement("ul");
 
-    var nameContainer = createElement("li", null, {}, {"textContent": item['name']});
-    basicDataContainer.appendChild(nameContainer);
+    //var nameContainer = createElement("li", null, {}, {"textContent": item['name']});
+    //basicDataContainer.appendChild(nameContainer);
+    //
+    //var desc = createElement("li", null, {}, {"textContent": item['details']['description']});
+    //basicDataContainer.appendChild(desc);
+    //
+    //var fileTypesContainer = document.createElement("ul");
+    //for (var j = 0; j < item['filesTypes'].length; j++) {
+    //    var fileType = item['filesTypes'][j];
+    //    var fileTypeNode = createElement("li", null, {}, {"textContent": fileType["filename"]});
+    //    fileTypesContainer.appendChild(fileTypeNode);
+    //}
+    //basicDataContainer.appendChild(fileTypesContainer);
 
-    var desc = createElement("li", null, {}, {"textContent": item['details']['description']});
-    basicDataContainer.appendChild(desc);
-
-    var fileTypesContainer = document.createElement("ul");
-    for (var j = 0; j < item['filesTypes'].length; j++) {
-        var fileType = item['filesTypes'][j];
-        var fileTypeNode = createElement("li", null, {}, {"textContent": fileType["filename"]});
-        fileTypesContainer.appendChild(fileTypeNode);
-    }
-    basicDataContainer.appendChild(fileTypesContainer);
-
+    var properties = {
+        "name": {
+            "type": "primitive",
+            "value": "name",
+            "element": {
+                "element": "li",
+                "container": null,
+                "attributes": {},
+                "properties": {}
+            }
+        },
+        "details": {
+            "type": "object",
+            "value": {
+                "description": {
+                    "type": "primitive",
+                    "value": "description",
+                    "element": {
+                        "element": "li",
+                        "container": null,
+                        "attributes": {},
+                        "properties": {}
+                    }
+                }
+            },
+            "element": {
+                "element": "ul",
+                "container": "li",
+                "attributes": {},
+                "properties": {}
+            }
+        },
+        "filesTypes": {
+            "type": "array",
+            "value": {
+                "filename": {
+                    "type": "primitive",
+                    "value": "filename",
+                    "element": {
+                        "element": "li",
+                        "container": null,
+                        "attributes": {},
+                        "properties": {}
+                    }
+                },
+                "extension": {
+                    "type": "primitive",
+                    "value": "extension",
+                    "element": {
+                        "element": "li",
+                        "container": null,
+                        "attributes": {},
+                        "properties": {}
+                    }
+                },
+                "distributions": {
+                    "type": "array",
+                    "value" : {
+                        "name": {
+                            "type": "primitive",
+                            "value": "name",
+                            "element": {
+                                "element": "li",
+                                "container": null,
+                                "attributes": {},
+                                "properties": {}
+                            }
+                        },
+                        "price": {
+                            "type": "primitive",
+                            "value": "price",
+                            "element": {
+                                "element": "li",
+                                "container": null,
+                                "attributes": {},
+                                "properties": {}
+                            }
+                        },
+                    },
+                    "separator": {
+                        "element": "ul",
+                        "container": "li"
+                    },
+                    "element": {
+                        "element": "ul",
+                        "container": "li",
+                        "attributes": {},
+                        "properties": {}
+                    }
+                }
+            },
+            "separator": {
+                "element": "ul",
+                "container": "li"
+            },
+            "element": {
+                "element": "ul",
+                "container": "li",
+                "attributes": {},
+                "properties": {}
+            }
+        }
+    };
+    printItemProperties(item, properties, basicDataContainer);
     var checkbox = createElement("input", "li", {
         "type": "checkbox",
         "class": "itemCheckbox"
